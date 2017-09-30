93ae7e00e3689967c1481e7760393b89c78ae6c7
Adam Bien <abien@adam-bien.com>
2017-09-30 15:32:17 -0700

    buildscript and README are generated

---
+++ src/main/resources/archetype-resources/buildAndRun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mvn clean package && docker build -t ${groupId}/${artifactId} .
+docker rm -f ${artifactId} || true && docker run -d -p 8080:8080 -p 4848:4848 --name ${artifactId} ${groupId}/${artifactId} 
